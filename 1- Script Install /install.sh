add5b8d9bc4255cf90a646f8d79dc00f0242373f
PedroCardenete <pedro.cardenete@gmail.com>
2022-04-19 21:14:29 -0300

    Initial commit

---
+++ 1- Script Install /install.sh
@@ -0,0 +1,56 @@
+# cURL install
+
+sudo apt-get install curl
+
+# NVM install
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Check NVM install
+
+command -v nvm
+
+# Check NVM list
+
+nvm ls
+
+# Node.js LTS install
+
+nvm install --lts
+
+# Check NVM list
+
+nvm ls
+
+# Install Serverless Framework
+
+npm install -g serverless
+
+or 
+
+curl -o- -L https://slss.io/install | bash
+
+# Serverless Plugin
+
+npm install -g serverless-pseudo-parameters
+
+# Work repository start (Serveless Framework)
+
+sls create --template "aws-python3"
+
+# Python Virtual Env & Python and Pip Install
+
+apt install python3
+
+apt install python3-pip
+
+pip3 install virtualenv && python3 -m venv ~/venv
+
+# Env Activate
+
+source ~/venv/bin/activate
+
+# Boto3 
+
+pip3 install boto3
+
---
+++ 2- Serverless/handler.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.insert(0, '/opt')
+
+import boto3
+import json
+
+client = boto3.client('s3')
+
+
+def handler(event, context):
+    print(json.dumps(event))
+    print(type(event))
+    print(event.keys())
+
+
+    bucket=event["bucket"]
+    chave=event["key"]
+
+    print(bucket)
+
+
+    response = client.get_object(
+        Bucket=bucket,
+        Key=chave,
+    )
+    print(response)
+
+    return response['ResponseMetadata']['HTTPStatusCode']
